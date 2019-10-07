e705aabfa69a419e14c858848139d99bad8a3ae9
Prithula <sharmilarahmanprithula@gmail.com>
2019-10-07 13:53:00 +0600

    init commit

---
+++ app/src/main/java/com/example/navigation/Gyroclass.java
@@ -0,0 +1,15 @@
+package com.example.navigation;
+
+public class Gyroclass {
+String str;
+    public Gyroclass() {
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+}
